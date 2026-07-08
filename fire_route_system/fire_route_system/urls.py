1ebe8189cc4329bffad11b06f310895703a7afce
ThiriMyatNoe-developer <thirimyatnoe.developer@gmail.com>
2026-07-08 10:11:13 +0630

    feat: clear conflict

--- fire_route_system/fire_route_system/urls.py
+++ fire_route_system/fire_route_system/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-<<<<<<< HEAD
+
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('firestation.urls')),
 ]
-=======
 from django.urls import path
 from Tbl_firereport import views
 
@@ -14,4 +13,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('report/',views.report_fire,name='report_fire'),
 ]
->>>>>>> 81f10ca2afee6271e0ff571967e62f586f43af53
