5d97357ae5c2a6001f45f9f6ec6f21ddfbc61e57
thirilinn-coder <thirilinn.developer@gmail.com>
2026-07-10 10:45:44 +0630

    feat: Tbl_user CRUD

--- fire_route_system/Tbl_user/views.py
+++ fire_route_system/Tbl_user/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Role, User
+from .forms import RoleForm, UserForm
 
-# Create your views here.
+
+# ROLE CRUD
+
+
+def role_list(request):
+    roles = Role.objects.all()
+    return render(request, 'role/list.html', {'roles': roles})
+
+
+def role_create(request):
+    form = RoleForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('role_list')
+
+    return render(request, 'role/form.html', {'form': form})
+
+
+def role_update(request, pk):
+    role = get_object_or_404(Role, pk=pk)
+
+    form = RoleForm(request.POST or None, instance=role)
+
+    if form.is_valid():
+        form.save()
+        return redirect('role_list')
+
+    return render(request, 'role/form.html', {'form': form})
+
+
+def role_delete(request, pk):
+    role = get_object_or_404(Role, pk=pk)
+
+    if request.method == 'POST':
+        role.delete()
+        return redirect('role_list')
+
+    return render(request, 'role/delete.html', {'role': role})
+
+
+# USER CRUD
+
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'user/list.html', {'users': users})
+
+
+def user_create(request):
+    form = UserForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('user_list')
+
+    return render(request, 'user/form.html', {'form': form})
+
+
+def user_update(request, pk):
+    user = get_object_or_404(User, pk=pk)
+
+    form = UserForm(request.POST or None, instance=user)
+
+    if form.is_valid():
+        form.save()
+        return redirect('user_list')
+
+    return render(request, 'user/form.html', {'form': form})
+
+
+def user_delete(request, pk):
+    user = get_object_or_404(User, pk=pk)
+
+    if request.method == 'POST':
+        user.delete()
+        return redirect('user_list')
+
+    return render(request, 'user/delete.html', {'user': user})
--- fire_route_system/fire_route_system/urls.py
+++ fire_route_system/fire_route_system/urls.py
@@ -4,12 +4,22 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('firestation.urls')),
+    path('', include('Tbl_firestation.urls')),
 ]
+
+
 from django.urls import path
 from Tbl_firereport import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('report/',views.report_fire,name='report_fire'),
+]
+
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('Tbl_user.urls')),
 ]
